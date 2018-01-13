97d4c37b803b7e417af91de942414dd35ac99b90
Leticia <letribeiro_1@hotmail.com>
2018-01-13 14:01:53 -0200

    Import correto

--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -2,17 +2,17 @@ package br.com.caelum.ingresso.validacao;
 
 import java.time.Duration;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
-import junit.framework.Assert;
+
 
 public class GerenciadorDeSessaoTest {
 
